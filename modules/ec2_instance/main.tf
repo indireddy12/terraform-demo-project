3a55ddf16b197830ef33075dd45d3790da9cc30b
Nivas Reddy <indireddysrinivasulareddy@gmail.com>
2025-04-02 22:24:06 +0530

    added terraform module

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "instance1" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+}
---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,6 @@
+variable "ami_value" {
+    description = "Provide ami_id"
+}
+variable "instance_type_value" {
+    description = "Provide instance type value"
+}
