b2a0b5b350bae6d235aba521a44bc08c02812b53
Indireddy Srinivasula Reddy <57956093+indireddy12@users.noreply.github.com>
2025-04-04 13:16:16 +0530

    Update variables.tf

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,6 +1,8 @@
 variable "ami_value" {
     description = "Provide ami_id"
+    type = "string"
 }
 variable "instance_type_value" {
     description = "Provide instance type value"
+    type = string
 }
